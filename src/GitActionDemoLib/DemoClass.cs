b5752c63e6cdb875dc30d22bbb4eba3aadd9f469
Shrikant Bhatt <sbhatt20@dxc.com>
2020-09-17 17:20:42 +0530

    Initial Chaeckin

---
+++ src/GitActionDemoLib/DemoClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GitActionDemoLib
+{
+    public class DemoClass
+    {
+    public string DemoMethod(string testData)
+    {
+      return "this is a demo method output-" + testData;
+    }
+  }
+}
